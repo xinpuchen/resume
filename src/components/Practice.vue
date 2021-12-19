9e2b6326feb4a867bb8db5271dfc954c81ee2759
xinpu <xinpuchen@126.com>
2021-12-20 01:49:20 +0800

    chore: updated with  November 28, 2021

--- src/components/Practice.vue
+++ src/components/Practice.vue
@@ -15,20 +15,17 @@
         </header>
         <div class="item-bd">
           <p class="item-des">
-            围绕以 <a href="https://www.nanguakexue.com/">南瓜科学APP</a> 为核心的应用生态的前端开发，包括：
+            参与以 <a href="https://www.nanguakexue.com/">南瓜科学APP</a> 生态为核心的前端开发，包括：
           </p>
           <ul class="section-content">
             <li>
-              参与后台管理系统的开发，包括课程、用户、订单、活动等模块，对产品运营起数据支撑的作用
+              负责电商增长业务的前端开发，包括基础电商及营销、策略运营与增长类活动等
             </li>
             <li>
-              参与教师管理平台的开发，包括老师、学员、班级、数据看板等模块，对教务运营起管理支撑的作用
+              参与后台管理系统的开发，包括课程、商品、订单、活动等模块，对产品运营起数据支撑的作用
             </li>
             <li>
-              参与各平台投放落地页的开发，其中主要涉及到支付模块的接入、兼容和整合处理
-            </li>
-            <li>
-              负责用户增长相关业务的前端开发，包括分享裂变、抽奖活动、平台合作等
+              参与教师管理平台的开发，包括老师、学员、班级、数据等模块，对辅导运营起管理支撑的作用
             </li>
           </ul>
         </div>
@@ -38,24 +35,42 @@
           <h3 class="item-name">
             青云QingCloud - 前端开发工程师
           </h3>
-          <a class="item-time" href="https://www.qingcloud.com/">2018.07~2020.11</a>
+          <a class="item-time" href="https://www.qingcloud.com/">2019.06~2020.11</a>
         </header>
         <div class="item-bd">
           <p class="item-des">
-            参与 <a href="https://www.npmjs.com/package/@pitrix/lego-ui">lego-UI</a> 组件库和负责 <a href="https://www.qingcloud.com/products/express/">超融合云平台</a> 的前端开发，包括：
+            负责 <a href="https://www.qingcloud.com/products/express/">超融合云平台</a> 项目的前端开发和前端进度把控，包括：
+          </p>
+          <ul class="section-content">
+            <li>
+              超融合云平台前端代理负责人，协调团队与业务、产品、设计等配合，把握项目开发进度
+            </li>
+            <li>
+              负责项目技术方案的调研与选型，新技术探索与实践等
+            </li>
+            <li>
+              负责项目脚手架搭建、中间层搭建、构建流程及优化、版本控制及管理、CI/CD 等
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="item">
+        <header class="item-hd">
+          <h3 class="item-name">
+            青云QingCloud - 前端开发实习生
+          </h3>
+          <a class="item-time" href="https://www.qingcloud.com/">2018.07~2019.06</a>
+        </header>
+        <div class="item-bd">
+          <p class="item-des">
+            参与团队规范整合和 <a href="https://www.npmjs.com/package/@pitrix/lego-ui">lego-UI</a> 组件库的开发，包括：
           </p>
           <ul class="section-content">
             <li>
               参与前端团队开发流程和编码规范的制定并输出文档，统一团队代码规范，降低合作成本
             </li>
             <li>
-              lego-UI 组件库的开发和文档站点的建立，保证组件库的维护和团队间推广
-            </li>
-            <li>
-              超融合云平台前端代理负责人，协调团队与产品、业务、设计配合，把握项目开发进度
-            </li>
-            <li>
-              负责超融合云平台项目脚手架搭建、中间层搭建、构建流程及优化、版本控制及管理、CI/CD 等
+              参与 lego-UI 组件库的开发和文档站点的建立，保证组件库的开发、维护和团队间推广
             </li>
           </ul>
         </div>
